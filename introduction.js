11bee9a2209998cb80b17cd7f2c97a2f0fd5be98
swwayam <codingswayam24@gmail.com>
2023-01-05 07:53:35 +0530

    First js

---
+++ introduction.js
@@ -0,0 +1,19 @@
+// Data Types in js
+
+// Whole numbers are called Integers
+// Point values are called Floats
+// True and False Boolean
+// Text is called string
+
+console.log("Hi sanvi"); // "" or '' string
+console.log(10 + 10); // integer
+console.log(10.1 + .2); // float
+
+var userAge = prompt("Please enter your age"); // 10
+//alert("Hi there you have successfully logged in")
+
+if(userAge < 18){
+    alert("You are not elidigble");
+}else{
+    alert("You are eligible");
+}

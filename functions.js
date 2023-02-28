903a19fe57873c8b08f3542840aed84bfd51bc20
saanviuser <saanvinakirikanti@gmail.com>
2023-02-27 20:56:07 -0500

    saanvi code

--- functions.js
+++ functions.js
@@ -1,7 +1,33 @@
-function add(num1 , num2){
-    console.log(num1 + num2);
+function subtraction(num1,num2){
+    console.log(num1-num2);
 }
 
-add(23,24)
+subtraction(2,2)
+subtraction(233,433)
+subtraction(400,609874738294)
 
-// Task 1 - Create similar functions for sub,div,multiplication
+
+
+function division(num1,num2){
+    console.log(num1/num2);
+}
+
+division(5,6)
+division(6,6)
+division(37468590283740957685947499032847656473839475656,7595048374930465794732930394857564738393938474635282919)
+
+
+function multiplication(num1,num2){
+    console.log(num1*num2);
+}
+
+multiplication(86950,204029)
+multiplication(3,5)
+
+
+function addition(num1,num2){
+    console.log(num1+num2);
+}
+
+addition(1,1)
+addition(2,3)

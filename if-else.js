3d74da84cfbad733ae8c9962c8f158bc6db02804
swwayam <codingswayam24@gmail.com>
2023-03-10 07:42:57 +0530

    gg

---
+++ if-else.js
@@ -0,0 +1,26 @@
+// Js Sytnax // Syntax means a way of writing
+
+// if(condition){
+//     // some code
+// }else{
+//     // some code
+// }
+
+
+// var teslaModelX = 22
+// var teslaModelY = 10 
+
+
+// console.log(2+3);
+// console.log(teslaModelX + teslaModelY) // This means console.log("2" + "3")
+
+// console.log("1" + "2")
+// console.log("Sanvi" + " is doing classes")
+
+
+var percent = 100
+
+
+
+
+

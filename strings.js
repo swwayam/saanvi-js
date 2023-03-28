b578adc4d91f09ed19de868f1999f61c0e7b3994
swwayam <codingswayam24@gmail.com>
2023-03-28 07:33:13 +0530

    Strings

---
+++ strings.js
@@ -0,0 +1,22 @@
+const para = "Hi i am sanvi i stood 2nd in PI competition and got a frisbe"
+
+// Accessing the characters
+console.log(para[3]);
+
+// If you are accessing something you either use . or []
+
+// Know the length of the string
+console.log(para.length)
+
+// Accessing the last character
+console.log(para[para.length - 1]);
+
+
+// Getting the index of specific character
+
+// indexOf gives you the first index of the character that it finds.
+console.log(para.indexOf("P"));
+console.log(para[29]);
+
+// Getting the last character's index
+console.log(para.lastIndexOf("i"));

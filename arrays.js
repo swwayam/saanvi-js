0c15025006e3c43ca3c16544fbd7e94a0fad4862
swwayam <codingswayam24@gmail.com>
2023-04-18 06:57:28 +0530

    Task Arrays

---
+++ arrays.js
@@ -0,0 +1,9 @@
+let name1 = "sanvi"
+//.... till 100
+
+// To store multiple values we use arrays.
+let person =  ["Sanvi", 12, "Lamborghini"] // Creating an array
+
+console.log(person[2]);
+
+// Create a array with the names of your 5 favorite sports
